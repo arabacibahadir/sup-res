f76e2b116c3c3ac7338d1acb377f0af5a409dea9
arabacibahadir <bahadirarabaci@yandex.com>
2021-08-30 01:51:52 +0300

    Fix

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import pandas_ta as ta
 
 
 # rest ile api yaz-kripto seçmece yap
-# class ata bazi şeylere
+
 # ilk sitenin web sayfa tanıtımı var onu kendim sildim belki if yazıp o sitelinn linki varsa sil kodu yazabilirim--csvdeki ilk satırı sil,sildiyse devam silmediyse sil
 def main():
-    df = pd.read_csv("BTC.csv", delimiter=',', encoding="utf-8-sig", index_col=False, nrows=254)  # nrows kaç tane veriyi incelediğini gösteriyor onu kullanıcıya seçtirebiliriz mi?
+    # nrows -> number of candlesticks
+    df = pd.read_csv("BTC.csv", delimiter=',', encoding="utf-8-sig", index_col=False, nrows=254)
     df = df.iloc[::-1]
     df['date'] = pd.to_datetime(df['date'], format="%Y-%m-%d")
     df.reset_index(drop=True, inplace=True)
-    # REV OLANLARI NULL GELENLERİ SİLEREK HALLEDEBİLİRİZ TEK LİSTTEN YAPMAK İÇİN. BÖYLE GÜZEL DURMUYOR
     volume = list(reversed((df['Volume USDT'])))
     sma10 = list((df.ta.sma(10)))
     sma50 = list((df.ta.sma(50)))
@@ -55,12 +55,11 @@ def main():
         if support(df, row, 3, 1):
             ss.append((row, df.low[row], 1))
         if resistance(df, row, 3, 1):
-            rr.append((row, df.high[row], 1))  # if birbirlerine çok yakınlarsa diğerini sil ya da yazı olarak yazma ya
-            # da sadece 1 tanesini yaz
-            # eğer dirençler birbirleri çok yakınsa x noktalarını biraz sağa sola kaydır??????????
+            rr.append((row, df.high[row], 1))
+            # eğer dirençler birbirleri çok yakınsa x noktalarını biraz sağa sola kaydır???
 
     c = 0
-    # Draw Support lines
+    # Drawing support lines
     while 1:
         if c > len(ss) - 1:
             break
@@ -71,11 +70,11 @@ def main():
                       line=dict(color="LightSeaGreen", width=2))
         # Support annotations
         fig.add_annotation(x=len(df) + 10, y=ss[c][1], text=str(ss[c][1]),
-                           font=dict(size=12, color="LightSeaGreen"))
+                           font=dict(size=15, color="LightSeaGreen"))
 
         c += 1
 
-    # Draw Resistance lines
+    # Drawing resistance lines
     c = 0
     while 1:
         if c > len(rr) - 1:
@@ -87,76 +86,37 @@ def main():
                       line=dict(color="MediumPurple", width=1))
         # Resistance annotations
         fig.add_annotation(x=len(df) + 30, y=rr[c][1], text=str(rr[c][1]),
-                           font=dict(
-
-                               size=14,
-                               color="MediumPurple"
-                           ))
+                           font=dict(size=15, color="MediumPurple"))
 
         c += 1
 
-    # buraya class ata
     # Legend -> Resistance
     fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name="Resistance",
-        mode="lines",
-        marker=dict(color="MediumPurple", size=10)
+        y=[ss[0]], name="Resistance", mode="lines", marker=dict(color="MediumPurple", size=10)
     ))
     # Legend -> Support
     fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name="Support",
-        mode="lines",
-        marker=dict(color="LightSeaGreen", size=10)
+        y=[ss[0]], name="Support", mode="lines", marker=dict(color="LightSeaGreen", size=10)
     ))
     # Legend -> Current Resistance
     fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name=f"Current Resistance : {int(rr[-1][1])}",
-        mode="markers+lines",
-        marker=dict(color="MediumPurple", size=10)
+        y=[ss[0]], name=f"Current Resistance : {int(rr[-1][1])}", mode="markers+lines", marker=dict(color="MediumPurple", size=10)
     ))
     # Legend -> Current Support
     fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name=f"Current Support : {int(ss[-1][1])}",
-        mode="markers+lines",
-        marker=dict(color="LightSeaGreen", size=10)))
+        y=[ss[0]], name=f"Current Support : {int(ss[-1][1])}", mode="markers+lines", marker=dict(color="LightSeaGreen", size=10)))
 
     fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name=f" ",
-        mode="markers",
-        marker=dict(color="#E2E2E2", size=0)))
+        y=[ss[0]], name=f" -------------------------- ", mode="markers", marker=dict(color="#f5efc4", size=0)))
 
     fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name=f" -------------------------- ",
-        mode="markers",
-        marker=dict(color="#E2E2E2", size=0)))
+        y=[ss[0]], name=f"Indicators", mode="markers", marker=dict(color="#f5efc4", size=14)))
 
     fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name=f" ",
-        mode="markers",
-        marker=dict(color="#E2E2E2", size=0)))
+        y=[ss[0]], name=f"RSI : {int(rsi[100])}", mode="lines", marker=dict(color="#f5efc4", size=10)))
 
     fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name=f"Indicators",
-        mode="markers",
-        marker=dict(color="#E2E2E2", size=14)))
-    fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name=f"RSI : {int(rsi[100])}",
-        mode="lines",
-        marker=dict(color="#E2E2E2", size=10)))
-    fig.add_trace(go.Scatter(
-        y=[ss[0]],
-        name=f"Volume : {int(volume[1])} $ ",
-        mode="lines",
-        marker=dict(color="#E2E2E2", size=10)))
+        y=[ss[0]], name=f"Volume : {int(volume[1])} $ ", mode="lines", marker=dict(color="#f5efc4", size=10)))
 
     fig.add_trace(go.Scatter(x=df['date'].dt.strftime('%b-%d-%y'), y=sma10, name=f"SMA10 : {int(sma10[-1])}",
                              line=dict(color='#5c6cff', width=3)))
@@ -166,19 +126,16 @@ def main():
                              line=dict(color='#a69b05', width=3)))
 
     # fib levelleri???#fib levelleri???#fib levelleri???
-    # fib levelleri???#fib levelleri???#fib levelleri???
 
     # Chart updates
-    fig.update_layout(title=str(df['symbol'][0] + ' Daily Chart'), hovermode='x', dragmode="zoom", width=1800,
-                      height=1350,
-                      xaxis_title="Date", yaxis_title="Price", legend_title="Legend", legend=dict(bgcolor='#E2E2E2'))
-
+    fig.update_layout(title=str(df['symbol'][0] + ' Daily Chart'), hovermode='x', dragmode="zoom", width=1820,
+                      height=1225, plot_bgcolor='rgba(0,0,0,0)',
+                      xaxis_title="Date", yaxis_title="Price", legend_title="Legend",
+                      legend=dict(bgcolor='#f5efc4', x=0.03, y=1, traceorder="normal"))
     fig.update_xaxes(showspikes=True, spikecolor="green", spikethickness=2)
     fig.update_yaxes(showspikes=True, spikecolor="green", spikethickness=2)
 
     fig.show()
-
-    # main kısmını ekle program başlamadan önce website adresini islsin
 
     # ilk 5 direnç ilk 3 supportu yazsın karışmasın ortalık?
     # candle pattern?
