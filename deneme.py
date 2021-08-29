0f718e91e108594a89060074a8722355ed63fa15
arabacibahadir <bahadirarabaci@yandex.com>
2021-08-29 21:31:53 +0300

    Added files

---
+++ deneme.py
@@ -0,0 +1,14 @@
+import plotly.graph_objects as go
+
+import pandas as pd
+from datetime import datetime
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['AAPL.Open'],
+                high=df['AAPL.High'],
+                low=df['AAPL.Low'],
+                close=df['AAPL.Close'])])
+
+fig.show()
