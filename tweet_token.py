83039e23f58c0f2822e402bd7caa787b2f26a88d
arabacibahadir <bahadirarabaci@yandex.com>
2021-08-31 22:29:51 +0300

    token added

---
+++ tweet_token.py
@@ -0,0 +1,9 @@
+
+# This is just a sample file. You need to
+# edit this file. You need to get these
+# details from your Twitter app settings.
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
