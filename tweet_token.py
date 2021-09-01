29bcaf757d300b3360ed1b48663bfd53d21b6255
arabacibahadir <bahadirarabaci@yandex.com>
2021-09-01 20:38:10 +0300

    tweets deleted

--- tweet_token.py
+++
@@ -1,9 +0,0 @@
-
-# This is just a sample file. You need to
-# edit this file. You need to get these
-# details from your Twitter app settings.
-
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
