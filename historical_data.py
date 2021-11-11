c8f36e01288973fa371fb183dacccfed19adaf8e
arabacibahadir <bahadirarabaci@yandex.com>
2021-11-11 20:20:16 +0300

    change symbol list var

--- historical_data.py
+++ historical_data.py
@@ -6,8 +6,7 @@ import pandas as pd
 duration = 1000  # milliseconds
 freq = 440  # Hz
 client = Client(binance_api.api, binance_api.secret)  # Your Binance api and secret key
-symbol_list = ["BTCUSDT"]  # Add which pairs do you want
-symbol = symbol_list[0]
+symbol_list = ["BTCUSDT"]  # Pair
 file_name = symbol_list[0] + ".csv"
 
 
@@ -16,22 +15,22 @@ def hist_data():
                   'taker buy vol', 'taker buy quote vol', 'ignore']
 
     def historical_Data_Write():
-        csvFileW = open(symbol + ".csv", "w", newline='')
+        csvFileW = open(symbol_list[0] + ".csv", "w", newline='')
         klines_writer = csv.writer(csvFileW, delimiter=",")
 
         for candlestick in candlesticks:
             klines_writer.writerow(candlestick)
 
         csvFileW.close()
-        df = pd.read_csv(symbol + ".csv")
+        df = pd.read_csv(symbol_list[0] + ".csv")
         df = df.iloc[::-1]
-        df.to_csv(symbol + ".csv", header=headerList, index=False)
-        df = pd.read_csv(symbol + ".csv")
+        df.to_csv(symbol_list[0] + ".csv", header=headerList, index=False)
+        df = pd.read_csv(symbol_list[0] + ".csv")
         date = pd.to_datetime(df['unix'], unit='ms')
         df.insert(1, 'date', date)
         del df['volume'], df['close time'], df['taker buy vol'], df['taker buy quote vol'], df['ignore'], df[
             'tradecount']
-        df.to_csv(symbol + ".csv", index=False)
+        df.to_csv(symbol_list[0] + ".csv", index=False)
 
     for s in symbol_list:
         print("Data writing: ", s)
