0bb268659b2d7db0603d5b1e46d30cc83edaa185
arabacibahadir <bahadirarabaci@yandex.com>
2021-11-11 12:36:06 +0300

    change unix timestamp to datetime in csv file

--- historical_data.py
+++ historical_data.py
@@ -2,13 +2,16 @@ from binance import Client
 import csv
 import binance_api
 import pandas as pd
+import datetime
+import time
 
 duration = 1000  # milliseconds
 freq = 440  # Hz
 
 client = Client(binance_api.api, binance_api.secret)  # Your Binance api and secret key
 symbol_list = ["BTCUSDT"]
-headerList = ['unix', 'open', 'high', 'low', 'close', 'volume', 'close time', 'quote asset volume', 'number of trades', 'taker buy vol', 'takerbuy quote vol','ignore']
+headerList = ['unix', 'open', 'high', 'low', 'close', 'volume', 'close time', 'quote asset volume', 'number of trades',
+              'taker buy vol', 'takerbuy quote vol', 'ignore']
 
 
 def historical_Data_Write():
@@ -16,13 +19,16 @@ def historical_Data_Write():
     klines_writer = csv.writer(csvFileW, delimiter=",")
 
     for candlestick in candlesticks:
-
         klines_writer.writerow(candlestick)
 
     csvFileW.close()
+    df = pd.read_csv(symbol + "btc.csv")
+    df.to_csv(symbol + "btc.csv", header=headerList, index=False)
+    df = pd.read_csv(symbol + "btc.csv")
+    df['unix'] = pd.to_datetime(df['unix'], unit='ms')
+    print(df.head(10))
+    df.to_csv(symbol + "btc.csv", index=False)
 
-    file = pd.read_csv(symbol + "btc.csv")
-    file.to_csv(symbol + "btc.csv", header=headerList, index=False)
 
 for symbol in symbol_list:
     print("Data writing: ", symbol)
