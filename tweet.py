9113d2378789f90750decce9a2c17b9ada1cf402
arabacibahadir <bahadirarabaci@yandex.com>
2021-12-15 23:00:09 +0300

    change handle name

--- tweet.py
+++ tweet.py
@@ -16,5 +16,5 @@ def send_tweet(media, tweet):
 
 
 def is_image_tweet():
-    stat = api.user_timeline(screen_name='@sup_res', count=1)
+    stat = api.user_timeline(screen_name='@HANDLE-NAME', count=1)
     return stat[0]
