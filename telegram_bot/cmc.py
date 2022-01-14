8c9179965ea0a31c5ea2a6260acee5b8e178faef
arabacibahadir <bahadirarabaci@yandex.com>
2022-01-14 21:26:33 +0300

    init coinmarketcap top bar scraper

---
+++ telegram_bot/cmc.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def market():
+    URL = "https://coinmarketcap.com"
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    info = soup.find("div", class_="cmc-global-stats__inner-content")
+    t = []
+    for i in info:
+        t.append(i.text.replace('\xa0', ' '))
+
+    return "\n".join(t)
