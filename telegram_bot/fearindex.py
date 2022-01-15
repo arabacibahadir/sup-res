73058bf19d63111c32860e6da3994c08d8da7065
arabacibahadir <bahadirarabaci@yandex.com>
2022-01-15 14:51:57 +0300

    init fear func

---
+++ telegram_bot/fearindex.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def fear():
+    URL = "https://alternative.me/crypto/fear-and-greed-index/"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    info = soup.find_all("div", class_="fng-circle")
+    t = []
+    for i in info:
+        t.append(i.text)
+
+    return f"Fear&Greed Index\nNow:{t[0]}\nYesterday:{t[1]}\nLast Week:{t[2]}\nLast Month:{t[3]}\n"
+
