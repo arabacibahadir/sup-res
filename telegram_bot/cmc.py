8409f272d37901e1520de0b46ddd24eb1bda5105
arabacibahadir <bahadirarabaci@yandex.com>
2022-01-15 14:51:42 +0300

    delete line

--- telegram_bot/cmc.py
+++ telegram_bot/cmc.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 def market():
     URL = "https://coinmarketcap.com"
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, "html.parser")
     info = soup.find("div", class_="cmc-global-stats__inner-content")
     t = []
