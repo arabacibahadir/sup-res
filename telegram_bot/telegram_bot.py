003e7f4f310d6399d3b22f7c821d21d9b5ba8242
arabacibahadir <bahadirarabaci@yandex.com>
2022-01-16 13:56:52 +0300

    change sma's int to float for decimal pairs

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -297,11 +297,11 @@ def main():
         marker=dict(color=legend_color, size=10)))
 
     if time_frame in hist_htf:
-        fig.add_trace(go.Scatter(x=df['date'].dt.strftime('%b-%d-%y'), y=sma10, name=f"SMA10     : {int(sma10[-1])}",
+        fig.add_trace(go.Scatter(x=df['date'].dt.strftime('%b-%d-%y'), y=sma10, name=f"SMA10     : {float(sma10[-1]):,.2f}",
                                  line=dict(color='#5c6cff', width=3)))
-        fig.add_trace(go.Scatter(x=df['date'].dt.strftime('%b-%d-%y'), y=sma50, name=f"SMA50     : {int(sma50[-1])}",
+        fig.add_trace(go.Scatter(x=df['date'].dt.strftime('%b-%d-%y'), y=sma50, name=f"SMA50     : {float(sma50[-1]):,.2f}",
                                  line=dict(color='#950fba', width=3)))
-        fig.add_trace(go.Scatter(x=df['date'].dt.strftime('%b-%d-%y'), y=sma100, name=f"SMA100   : {int(sma100[-1])}",
+        fig.add_trace(go.Scatter(x=df['date'].dt.strftime('%b-%d-%y'), y=sma100, name=f"SMA100   : {float(sma100[-1]):,.2f}",
                                  line=dict(color='#a69b05', width=3)))
     elif time_frame in hist_ltf:
         fig.add_trace(
