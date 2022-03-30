200d273d5ec732e6fcc328431a5260dcba39e4c7
Bahadir Arabaci <bahadirarabaci@yandex.com>
2022-03-30 10:55:09 +0300

    Update telegram_main.py

--- telegram_bot/telegram_main.py
+++ telegram_bot/telegram_main.py
@@ -27,6 +27,8 @@ def handle_message(update, context):
     r_text = responses(text)
     update.message.reply_text(r_text)
 
+def get_alarm_data():
+    pass
 
 def responses(input_text):
     user_message = str(input_text).lower()
