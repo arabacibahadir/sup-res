3c4056d8a86b4a1ae7d09dd41b3168f7c39e285c
arabacibahadir <bahadirarabaci@yandex.com>
2022-07-11 16:49:00 +0300

    add last column to db

--- webapp/db_pandas.py
+++ webapp/db_pandas.py
@@ -1,7 +1,9 @@
 from binance.client import Client
 import pandas as pd
+import time
 
-alarm_list = 'alarm1', 'alarm2', 'alarm3', 'alarm4', 'alarm5', 'alarm6', 'alarm7', 'alarm8', 'alarm9', 'alarm10'
+alarm_list = 'alarm1', 'alarm2', 'alarm3', 'alarm4', 'alarm5', 'alarm6', 'alarm7', 'alarm8', 'alarm9', 'alarm10', \
+             'alarm_count'
 
 
 def create_db():
@@ -11,7 +13,7 @@ def create_db():
 
     alarm_data_columns = {'symbol': str(), 'alarm1': float(), 'alarm2': float(), 'alarm3': float(), 'alarm4': float(),
                           'alarm5': float(), 'alarm6': float(), 'alarm7': float(), 'alarm8': float(), 'alarm9': float(),
-                          'alarm10': float()}
+                          'alarm10': float(), 'alarm_count': int()}
     alarm_data = pd.DataFrame(df['symbol'], columns=alarm_data_columns)
     for _ in alarm_list:
         alarm_data.fillna({_: 0}, inplace=True)
@@ -38,6 +40,7 @@ def add_alarm_data(self: str, alarm: float):
 
             if coin_dict[i] == 0:
                 alarm_db.at[coin.index[0], i] = alarm
+                alarm_db.at[coin.index[0], 'alarm_count'] += 1
                 alarm_db.to_csv('alarm_data.csv', index=False)
                 return print(f"{self}: {alarm} added")
 
@@ -57,22 +60,22 @@ def remove_alarm_data(self: str, alarm: float):
 
 
 # TESTING
-create_db()
-get_live_price('BTCUSDT')
-add_alarm_data('BTCUSDT', 1000)
-add_alarm_data('ETHBTC', 3000)
-add_alarm_data('ETHBTC', 1000)
-add_alarm_data('ETHBTC', 2000)
-add_alarm_data('ETHBTC', 3003)
-add_alarm_data('ETHBTC', 3004)
-add_alarm_data('ETHBTC', 3005)
-add_alarm_data('ETHBTC', 3005)
-add_alarm_data('ETHBTC', 3022)
-add_alarm_data('ETHBTC', 3034)
-add_alarm_data('ETHBTC', 3067)
-add_alarm_data('ETHBTC', 3090)
-add_alarm_data('ETHBTC', 3007)
-add_alarm_data('ETHBTC', 3007)
+# create_db()
+# get_live_price('BTCUSDT')
+# add_alarm_data('BTCUSDT', 1000)
+# add_alarm_data('ETHBTC', 3000)
+# add_alarm_data('ETHBTC', 1000)
+# add_alarm_data('ETHBTC', 2000)
+# add_alarm_data('ETHBTC', 3003)
+# add_alarm_data('ETHBTC', 3004)
+# add_alarm_data('ETHBTC', 3005)
+# add_alarm_data('ETHBTC', 3005)
+# add_alarm_data('ETHBTC', 3022)
+# add_alarm_data('ETHBTC', 3034)
+# add_alarm_data('ETHBTC', 3067)
+# add_alarm_data('ETHBTC', 3090)
+# add_alarm_data('ETHBTC', 3007)
+# add_alarm_data('ETHBTC', 3007)
 
 # remove_alarm_data('ETHBTC', 2000)
 # remove_alarm_data('BTCUSDT', 1000)
