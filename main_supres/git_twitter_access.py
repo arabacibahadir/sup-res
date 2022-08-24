26adc1870aa1d05cdf186c620763849429ec23cf
arabacibahadir <bahadirarabaci@yandex.com>
2022-08-24 08:30:52 +0300

    change api key names

--- main_supres/git_twitter_access.py
+++ main_supres/git_twitter_access.py
@@ -1,6 +1,6 @@
 # Twitter api keys -> https://developer.twitter.com/en/docs/twitter-api
-twitter_api = 'YOUR-API'
-twitter_key_secret = 'API-SECRET'
-twitter_token = 'TOKEN'
-twitter_token_secret = 'TOKEN-SECRET'
+twitter_api_key = 'YOUR-API'
+twitter_api_key_secret = 'API-SECRET'
+twitter_access_token = 'TOKEN'
+twitter_access_token_secret = 'TOKEN-SECRET'
 user_handle = '@HANDLE-NAME'
