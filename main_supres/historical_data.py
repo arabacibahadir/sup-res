0b2398612f5eb00c05816fb18c930bd06e5304d4
arabacibahadir <bahadirarabaci@yandex.com>
2022-12-05 01:21:30 +0300

    replace for loop with dataFrame constructor in get_historical_klines

--- main_supres/historical_data.py
+++ main_supres/historical_data.py
@@ -3,10 +3,9 @@ import pandas as pd
 from binance.client import Client
 import frameselect
 
-print("Ticker and Time Frame:")  # Example:"BTCUSDT 1H", "ETHBTC 3D", "BNBUSDT 15M"
+print("Ticker and Time Frame: ")  # Example:"BTCUSDT 1H", "ETHBTC 3D", "BNBUSDT 15M"
 ticker, frame_s = str(input().upper()).split()
-time_frame = frameselect.frame_select(frame_s)[0]
-start = frameselect.frame_select(frame_s)[1]
+time_frame, start = frameselect.frame_select(frame_s)
 # Creating a client object that is used to interact with the Binance API
 client = Client("", "")
 if any(ticker == i.get('symbol') for i in client.get_all_tickers()):  # Check pair is in Binance API
@@ -15,23 +14,16 @@ else:
     print("Pair is not in Binance API.")
     exit()
 file_name = ticker + ".csv"
-symbol_data = client.get_symbol_info(ticker)
-header_list = ('unix', 'open', 'high', 'low', 'close', 'volume', 'close time', 'Volume USDT', 'tradecount',
-               'taker buy vol', 'taker buy quote vol', 'ignore')
+header_list = ['unix', 'open', 'high', 'low', 'close', 'volume', 'close time', 'Volume USDT', 'tradecount',
+               'taker buy vol', 'taker buy quote vol', 'ignore']
 
 
 def historical_data_write(ticker_symbol):
     """
     Write the historical data to a csv file
     """
-    csv_file_w = open(file_name, "w", newline='')
-    klines_writer = csv.writer(csv_file_w, delimiter=",")
-    klines_writer.writerow(header_list)
-    for candlestick in reversed(client.get_historical_klines(symbol=ticker_symbol, interval=time_frame,
-                                                             start_str=start, limit=300)):
-        klines_writer.writerow(candlestick)
-    csv_file_w.close()
-    df = pd.read_csv(file_name)
+    df = pd.DataFrame(reversed(
+        client.get_historical_klines(symbol=ticker_symbol, interval=time_frame, start_str=start)), columns=header_list)
     # Converting the unix time to a readable date format for today
     date = pd.to_datetime(df['unix'], unit='ms')
     df.insert(1, 'date', date)
