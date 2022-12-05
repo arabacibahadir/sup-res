d8e32429e33cc3fde16c093f51f570976d4b98c3
arabacibahadir <bahadirarabaci@yandex.com>
2022-12-05 04:12:40 +0300

    change api type to dict

--- main_supres/git_twitter_access.py
+++ main_supres/git_twitter_access.py
@@ -1,6 +1,8 @@
 # Twitter api keys -> https://developer.twitter.com/en/docs/twitter-api
-twitter_api_key = 'YOUR-API'
-twitter_api_key_secret = 'API-SECRET'
-twitter_access_token = 'TOKEN'
-twitter_access_token_secret = 'TOKEN-SECRET'
+twitter_credentials = {
+    'api_key': 'YOUR-API',
+    'api_key_secret': 'API-SECRET',
+    'access_token': 'TOKEN',
+    'access_token_secret': 'TOKEN-SECRET'
+}
 user_handle = '@HANDLE-NAME'
