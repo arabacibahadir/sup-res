443eb61846c8313b46a59e43fa32a4454e257b83
arabacibahadir <bahadirarabaci@yandex.com>
2023-02-15 13:50:56 +0300

    remove global variables

--- telegram_bot/cmc.py
+++ telegram_bot/cmc.py
@@ -1,21 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-text, news_all = [], []
-
 
 def market():
     """
-    This function scrapes the market data from the coinmarketcap.com website and returns a string with
-    the market data
-    :return: A string of text.
+    This function scrapes the market data from the coinmarketcap.com website
+    and returns a string with the market data
+    :return: Market info text
     """
     URL = "https://coinmarketcap.com"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-    info = soup.find("div", class_="cmc-global-stats__inner-content")
-    for i in info:
-        text.append(i.text.replace("\xa0", " "))
+    market_info = soup.find("div", class_="cmc-global-stats__inner-content")
+    text = [i.text.replace("\xa0", " ") for i in market_info]
     return "\n".join(text)
 
 
@@ -27,12 +24,9 @@ def news():
     URL = "https://coinmarketcap.com/headlines/news/"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-    for a_href in soup.find_all("a", href=True):
-        text.append(a_href.text)
-    index = text.index("Headlines")
-    for _ in text[index : index + 15 : 2]:
-        news_all.append(_)
-    news_list = ["-" + sub for sub in news_all]
+    news_elements = soup.find_all("a", class_="sc-1eb5slv-0", href=True)
+    news_all = [e.text for e in news_elements]
+    news_list = ["-" + sub for sub in news_all[:15:2]]
     news_list.append(URL)
     return "\n".join(news_list)
 
@@ -43,12 +37,14 @@ def fear():
     :return: A string of Fear&Greed Index data to telegram-bot.
     """
     URL = "https://alternative.me/crypto/fear-and-greed-index/"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-    info = soup.find_all("div", class_="fng-circle")
-    for i in info:
-        text.append(i.text)
-    return (
-        f"Fear&Greed Index:\nNow: {text[0]}\nYesterday: {text[1]}\n"
-        f"Last Week: {text[2]}\nLast Month: {text[3]}\n"
-    )
+    try:
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        info = soup.find_all("div", class_="fng-circle")
+        text = [i.text for i in info]
+        return (
+            f"Fear&Greed Index:\nNow: {text[0]}\nYesterday: {text[1]}\n"
+            f"Last Week: {text[2]}\nLast Month: {text[3]}\n"
+        )
+    except requests.exceptions.RequestException:
+        return "Error retrieving Fear&Greed Index data."
