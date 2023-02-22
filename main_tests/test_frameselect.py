7d5f825ece8b1dd38f5a40ce3893193dbcd56deb
arabacibahadir <bahadirarabaci@yandex.com>
2023-02-22 15:43:43 +0300

    fix module import error

--- main_tests/test_frameselect.py
+++ main_tests/test_frameselect.py
@@ -1,29 +1,25 @@
-import pytest
-
-from main_supres import frameselect
+import sys
+import os
 from datetime import datetime, timedelta
 
-
-@pytest.fixture
-def frame_select_data():
-    today = datetime.now()
-    return [
-        ("1M", ("1m", (today - timedelta(minutes=260)).strftime("%d %B, %Y"))),
-        ("3M", ("3m", (today - timedelta(minutes=780)).strftime("%d %B, %Y"))),
-        ("5M", ("5m", (today - timedelta(minutes=1300)).strftime("%d %B, %Y"))),
-        ("15M", ("15m", (today - timedelta(minutes=3900)).strftime("%d %B, %Y"))),
-        ("30M", ("30m", (today - timedelta(minutes=7800)).strftime("%d %B, %Y"))),
-        ("1H", ("1h", (today - timedelta(hours=260)).strftime("%d %B, %Y"))),
-        ("2H", ("2h", (today - timedelta(hours=520)).strftime("%d %B, %Y"))),
-        ("4H", ("4h", (today - timedelta(hours=1040)).strftime("%d %B, %Y"))),
-        ("6H", ("6h", (today - timedelta(hours=1560)).strftime("%d %B, %Y"))),
-        ("8H", ("8h", (today - timedelta(hours=2080)).strftime("%d %B, %Y"))),
-        ("12H", ("12h", (today - timedelta(hours=3120)).strftime("%d %B, %Y"))),
-        ("1D", ("1d", (today - timedelta(days=260)).strftime("%d %B, %Y"))),
-        ("3D", ("3d", (today - timedelta(days=780)).strftime("%d %B, %Y"))),
-    ]
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../main_supres"))
+)
+from frameselect import frame_select, frame_select_dict
 
 
-def test_frameselect(frame_select_data):
-    for frame, expected in frame_select_data:
-        assert frameselect.frame_select(frame) == expected
+def test_frame_select():
+    for kline, values in frame_select_dict.items():
+        expected_kline_interval = values[0]
+        kline_interval = values[1]
+        last_letter = expected_kline_interval[-1].upper()
+        times = {
+            "M": timedelta(minutes=kline_interval),
+            "H": timedelta(hours=kline_interval),
+            "D": timedelta(days=kline_interval),
+            "W": timedelta(weeks=kline_interval),
+        }
+        expected_start_date = (datetime.now() + times[last_letter]).strftime(
+            "%d %B, %Y"
+        )
+        assert frame_select(kline) == (expected_kline_interval, expected_start_date)
